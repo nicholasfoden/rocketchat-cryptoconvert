1f6faf9609afab3eddcdfa031f22390b770de19a
nick <nicholasfoden@gmail.com>
2019-05-09 15:30:07 +0100

    add api key setting

--- CryptoVertApp.ts
+++ CryptoVertApp.ts
@@ -5,8 +5,11 @@ import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { CryptoVertConvertCommand } from "./CryptoVertConvertCommand";
 import { CryptoVertPriceCommand } from "./CryptoVertPriceCommand";
 import { CryptocompareAPI } from "./CryptocompareAPI";
+import { CryptoVertSettingAPI } from "./CryptoVertSettingAPI";
 
 export class CryptoVertApp extends App {
+
+    public api: CryptocompareAPI;
 
     constructor(
     		info: IAppInfo, 
@@ -18,6 +21,7 @@ export class CryptoVertApp extends App {
 
     public async initialize(configurationExtend: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void>{
     	this.getLogger().log('initialising cryptovert');
+        this.api = new CryptocompareAPI(environmentRead.getSettings())
     	await this.extendConfiguration(configurationExtend, environmentRead);
     }
 
@@ -25,11 +29,8 @@ export class CryptoVertApp extends App {
     		configuration: IConfigurationExtend, 
     		environmentRead: IEnvironmentRead
     ): Promise<void> {
-    	console.log("extendConfiguration")
-    	await configuration.slashCommands.provideSlashCommand(new CryptoVertConvertCommand(new CryptocompareAPI));
-    	await configuration.slashCommands.provideSlashCommand(new CryptoVertPriceCommand(new CryptocompareAPI));
+        await configuration.settings.provideSetting(new CryptoVertSettingAPI());
+    	await configuration.slashCommands.provideSlashCommand(new CryptoVertConvertCommand(this.api));
+    	await configuration.slashCommands.provideSlashCommand(new CryptoVertPriceCommand(this.api));
     }
-
-    //TODO add settings for API key
-
 }
--- CryptocompareAPI.ts
+++ CryptocompareAPI.ts
@@ -1,24 +1,38 @@
 import { IHttp, IModify, IPersistence, IRead, IHttpRequest, HttpStatusCode } from '@rocket.chat/apps-engine/definition/accessors';
-
+import { ISettingRead } from "@rocket.chat/apps-engine/definition/accessors/";
 
 export class CryptocompareAPI {
 
     //TODO add option for different API's
     private readonly url: string = 'https://min-api.cryptocompare.com/data/price';
+    private key: string;
+
+    constructor(private settings: ISettingRead){
+      this.setupKey(settings);
+      
+    }
+    /**
+     *  Sets the key based on the APP settings
+     * @param settings ISettingRead accessor (can be got from environmentRead)
+     */
+    private async setupKey(settings: ISettingRead){
+      this.key = await settings.getValueById("APIKEY")
+
+    }
 
     /**
      * Gets a price from the API for a given conversion
+     * @returns Object An object containing the price for the symbols requested
      */
     public async getPrice(
         http: IHttp, 
         from: string, 
-        to: string,
-        key: string
-    ): Promise<any> {
+        to: string
+    ): Promise<any> { 
 
     	// Create options for the request
       let options = {
-        header: { authorization: 'Apikey ' + key },
+        header: { authorization: 'Apikey ' + this.key },
         params: { fsym: from, tsyms:  to }
       }
 
